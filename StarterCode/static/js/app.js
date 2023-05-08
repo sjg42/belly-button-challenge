dd59139f576b8c8145c1f4dd4be0acf8609e49b4
sjg42 <samjgantman@gmail.com>
2023-05-08 11:30:46 -0400

    Commented and cleaned app.py
    Added additional comments and cleaned out unnecessary commented-out code.

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -23,11 +23,13 @@ function optionChanged()
         // d3 selector on the dropdown menu
         let dropdown = d3.select("#selDataset");
 
-        // access nested value properties from the options in teh dropdown
+        // access nested value property from the selected option
         let ID = dropdown.property("value");
         
+        //Loop through data.names and find the index of the selected ID
         for (i=0; i<data.names.length; i++)
-        {
+        {   
+            //run the functions for the ID selected
             if (data.names[i] == ID)
             {
                 demoInfo(data.metadata[i])
@@ -70,6 +72,7 @@ function populate_dropdown(jsonData)
 }
 
 //Populate Demographic Info container
+//Takes in the metadata of the ID selected
 function demoInfo(ID_meta)
 {
     output = `id: ${ID_meta.id} <br>
@@ -85,6 +88,7 @@ function demoInfo(ID_meta)
 
 
 //Create bar chart with selected ID - using Plotly.newPlot
+//Takes in the sample of the selected ID
 function barChart(ID)
 {
     //slice data
@@ -118,6 +122,7 @@ function barChart(ID)
 
 
 //Create bubble chart with selected ID - using Plotly.newPlot
+//Takes in the sample of the selected ID
 function bubbleChart(ID)
 {
     //make trace for bubble chart
@@ -143,71 +148,3 @@ function bubbleChart(ID)
 
     Plotly.newPlot("bubble",traceData,layout)
 }
-
-
-
-// //Create bar chart with selected ID - using Plotly.newPlot
-// function update_barChart(ID)
-// {
-//     //assign sample data to variables
-//     let sampleVals = ID.sample_values
-//     let otuIds = ID.otu_ids
-//     let otuLabels = ID.otu_labels
-
-//     //slice data
-//     let top10_sampleVals = sampleVals.slice(0,10)
-//     let top10_otuIds = otuIds.slice(0,10)
-//     let top10_otuLabels = otuLabels.slice(0,10)
-
-//     //reverse data
-//     top10_sampleVals.reverse()
-//     top10_otuIds.reverse()
-//     top10_otuLabels.reverse()
-
-//     //Change outIds to strings for plotting
-//     top10_otuIds_strings = top10_otuIds.map(val => {return "OTU " +String(val)})
-
-//     let x = top10_sampleVals
-//     let y = top10_otuIds_strings
-//     let text = top10_otuLabels
-
-//     Plotly.restyle("bar","x", [x])
-//     Plotly.restyle("bar","y", [y])
-//     Plotly.restyle("bar","text", [text])
-// }
-
-
-// //Create bubble chart with selected ID - using Plotly.newPlot
-// function update_bubbleChart(ID)
-// {
-//     let x = top10_sampleVals
-//     let y = top10_otuIds_strings
-//     let text = top10_otuLabels
-
-//     Plotly.restyle("bar","x", [x])
-//     Plotly.restyle("bar","y", [y])
-//     Plotly.restyle("bar","text", [text])
-    
-//     //make trace for bubble chart
-//     //Reference - https://plotly.com/javascript/bubble-charts/
-//     var trace = {
-//         x: ID.otu_ids,
-//         y: ID.sample_values,
-//         mode: 'markers',
-//         marker:{
-//             size: ID.sample_values,
-//             color: ID.otu_ids,
-//         },
-//         text: ID.otu_labels
-//     }
-
-//     var traceData = [trace]
-
-//     var layout = {
-//         xaxis:{
-//             title:{text: "OTU ID"}
-//         }
-//     }
-
-//     Plotly.restyle("bubble",traceData,layout)
-// }
